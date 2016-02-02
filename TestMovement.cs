c659185ad8e14bfe1bda99ba1ef04afd85132d06
Bruce <jackellice@gmail.com>
2016-02-02 17:35:03 +0800

    Level setup
    half of the colliders done. ghost added. now need to script it.

---
+++ TestMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestMovement : MonoBehaviour {
+	public float ghostSpeed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += new Vector3(Time.deltaTime * ghostSpeed * Input.GetAxis ("Horizontal"), 0,
+		                                       Time.deltaTime * ghostSpeed * Input.GetAxis("Vertical"));
+	
+	}
+}
